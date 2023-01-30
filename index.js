e7e6f96db714b557f34756d7cf2d4639379dbb5e
haw-ms <ms@webimpact.co.in>
2023-01-30 13:40:03 +0530

    updated

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use("/", Routes);
 
 // for production routes
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static('frontend/build'))
+    app.use(express.static('client/build'))
     const path = require('path')
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
